663eebbdb2b0fdf800a16facec9ec88f6ce2f7e9
David <>
2021-03-10 14:51:44 +0700

    one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 class App extends React.Component {
-  render() {
-    return (
-      <h1>dsf</h1>
-    )
-  }
+    render() {
+        return (
+            <h1>213</h1>
+        )
+    }
 }
 
 export default App
